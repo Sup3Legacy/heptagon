2b6b8264b062bbfcd2cd79840d21e570d83ad4f7
Brice Gelineau <brice.gelineau@m4x.org>
2011-07-29 15:39:54 +0200

    Bugfix for arrays : the between function sometimes crashed OpenCL (with Nvidia platform)

--- lib/opencl/pervasives.h
+++ lib/opencl/pervasives.h
@@ -3,8 +3,10 @@
 #ifndef DECADES_PERVASIVES_H
 #define DECADES_PERVASIVES_H
 
-/* between(i, n) returns idx between 0 and n-1. */
-#define between(idx,n) ((idx >= n) ? n-1 : (idx < 0 ? 0 : idx))
+/* between(i, n) returns idx between 0 and n-1.
+   Because of a bug who sometimes occurs with Nvidia (even though extremely
+   rare), we can't use the C version and return 0 when out of bounds. */
+#define between(idx,n) ((idx < n) && (idx >= 0) ? idx : 0)
 
 #endif
 
