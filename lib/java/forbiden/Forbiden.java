9037462fed17b322fb0428d344e4e658fa76871f
Leonard Gerard <leonard.gerard@ens.fr>
2012-03-25 17:56:37 +0200

    forbiden.java updated:
    a solution to the error thrown
    because of possible non initialized values (x here)
    is to add a default case with throw new AssertionError();

--- lib/java/forbiden/Forbiden.java
+++ lib/java/forbiden/Forbiden.java
@@ -5,16 +5,20 @@ import java.util.concurrent.Future;
 enum E { e1, e2 }
 
 public class Forbiden {
-	void array_of_generics () {
-		Future<Integer>[] m = new Future<Integer>[N];
-	}
+//	void array_of_generics () {
+//		Future<Integer>[] m = new Future<Integer>[N];
+//	}
 	int init_match (E e) {
 		int x;
 		switch(e) {
 		case e1 :
 			x = 0;
+			break;
 		case e2 :
 			x = 1;
+			break;
+		default :
+			throw new AssertionError();
 		}
 		return x;
 	}
