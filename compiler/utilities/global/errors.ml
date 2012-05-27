d320a315b57c04b3050478c84861cbd41a003699
Leonard Gerard <leonard.gerard@ens.fr>
2012-05-27 15:42:29 +0200

    Common Error handling proposition and Better cmain error

--- compiler/utilities/global/errors.ml
+++ compiler/utilities/global/errors.ml
@@ -9,5 +9,14 @@
 
 (* error during the whole process *)
 exception Error
+
+let errorf f p =
+	Format.eprintf (" Error : "^^f^^"@.") p;
+	raise Error
+
+let warningf f p =
+	Format.eprintf (" Warning : "^^f^^"@.") p;
+
+
 (** Ast iterators *)
 exception Fallback
