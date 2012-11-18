8ee2483e9afb99381caa2fa5830d254d23f16f1a
Léonard Gérard <leonard.gerard@ens.fr>
2012-11-18 04:02:35 +0100

    Calibrate do_stuff

--- lib/c/do.cpp
+++ lib/c/do.cpp
@@ -4,7 +4,7 @@
 void Do__stuff_step(int coeff) {
 	int x = 13;
 	for (int i = 0; i<coeff; i++) {
-		for (int j = 0; j<10000; j++) {
+		for (int j = 0; j<8; j++) {
 			x = (x+j) % (x+j/x) + 13;
 		}
 	}
