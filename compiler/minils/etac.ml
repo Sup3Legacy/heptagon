3482ab83150a7d49cb546f7a56eb1f565ea9e031
Valentin Lorentz <valentin.lorentz@ens-lyon.org>
2015-06-04 18:42:03 +0200

    [etac] Output node outputs.

--- compiler/minils/etac.ml
+++ compiler/minils/etac.ml
@@ -6,18 +6,26 @@ let string_of_modul (modul: Names.modul) =
     | Names.QualModule name -> aux (name.Names.name :: stack) name.Names.qual
   in String.concat "__" (List.rev (aux [] modul))
 
-let string_of_vardec (vardec: Minils.var_dec) =
+let string_of_vardec (with_type: bool) (vardec: Minils.var_dec) =
   (* TODO: handle types *)
-  String.concat " " ["i64"; Idents.name vardec.Minils.v_ident]
+  if with_type then
+    String.concat " " ["i64"; Idents.name vardec.Minils.v_ident]
+  else
+    Idents.name vardec.Minils.v_ident
 
 let node_pred file (node: Minils.node_dec) =
   (* Print node name *)
   Printf.fprintf file "node @%s__%s {\n"
       (string_of_modul node.Minils.n_name.Names.qual)
       node.Minils.n_name.Names.name;
+
   (* Print inputs *)
-  let inputs = List.map string_of_vardec node.Minils.n_input in
+  let inputs = List.map (string_of_vardec true) node.Minils.n_input in
   Printf.fprintf file "  %s = init;\n" (String.concat ", " inputs);
+
+  (* Print outputs *)
+  let inputs = List.map (string_of_vardec false) node.Minils.n_output in
+  Printf.fprintf file "  exit %s;\n" (String.concat ", " inputs);
 
   (* Print end of node *)
   Printf.fprintf file "}\n"
