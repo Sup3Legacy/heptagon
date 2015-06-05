26ed7fcd7f0d18b4707eac33e9dc5494453e512c
Valentin Lorentz <valentin.lorentz@ens-lyon.org>
2015-06-05 11:49:16 +0200

    [etac] Add basic support of instructions and clocks.

--- compiler/minils/etac.ml
+++ compiler/minils/etac.ml
@@ -1,10 +1,21 @@
 let string_of_modul (modul: Names.modul) =
   let rec aux stack = function
-    | Names.Pervasives -> "PERVASIVES" :: stack
+    | Names.Pervasives -> (
+        match (List.hd stack) with
+        | "+" -> "add" :: (List.tl stack)
+        | "-" -> "sub" :: (List.tl stack)
+        | "*" -> "mul" :: (List.tl stack)
+        | "/" -> "div" :: (List.tl stack)
+        (* TODO: binary operators *)
+        | _ -> "PERVASIVES" :: stack
+      )
     | Names.LocalModule -> "LOCALMODULE" :: stack
     | Names.Module name -> name :: stack
     | Names.QualModule name -> aux (name.Names.name :: stack) name.Names.qual
-  in String.concat "__" (List.rev (aux [] modul))
+  in String.concat "__" (aux [] modul)
+
+let string_of_qualname (qualname: Names.qualname) =
+  string_of_modul (Names.QualModule qualname)
 
 let string_of_varident (varident: Idents.var_ident) =
   Printf.sprintf "%%%s" (Idents.name varident)
@@ -16,6 +27,14 @@ let string_of_vardec (with_type: bool) (vardec: Minils.var_dec) =
   else
     string_of_varident vardec.Minils.v_ident
 
+let rec string_of_static_exp (sexp: Types.static_exp) =
+  string_of_static_exp_desc sexp.Types.se_desc
+
+and string_of_static_exp_desc = function
+  | Types.Svar name -> string_of_qualname name
+  | Types.Sint i -> string_of_int i
+  | _ -> "<static_exp_desc constructor not handled>" (* TODO *)
+
 let rec string_of_pat = function
   (* FIXME: not tail-recursive *)
   | Minils.Evarpat var_ident ->
@@ -24,11 +43,42 @@ let rec string_of_pat = function
       Printf.sprintf "%s" (String.concat ", " (List.map string_of_pat pats))
 
 let rec string_of_exp (exp: Minils.exp)  =
-  "foo"
+  string_of_edesc exp.Minils.e_desc
 
+and string_of_edesc = function
+  | Minils.Eextvalue ev -> string_of_extvalue ev
+  | Minils.Eapp (app, evs, None) ->
+      Printf.sprintf "%s %s"
+          (string_of_app app)
+          (String.concat ", " (List.map string_of_extvalue evs))
+  | Minils.Eapp (app, evs, Some varident) ->
+      Printf.sprintf "%s %s(%s)"
+          (string_of_app app)
+          (String.concat ", " (List.map string_of_extvalue evs))
+          (string_of_varident varident)
+  | _ -> "<edesc constructor not handled>" (* TODO *)
 
-let rec string_of_ck (ck: Clocks.ck) =
-  "bar"
+and string_of_app (app: Minils.app) =
+  match app.Minils.a_op with
+  | Minils.Efun f -> string_of_qualname f
+  | _ -> "<op constructor not handled>" (* TODO *)
+
+and string_of_extvalue (ev: Minils.extvalue) =
+  string_of_extvaluedesc ev.Minils.w_desc
+
+and string_of_extvaluedesc = function
+  | Minils.Wvar varident -> string_of_varident varident
+  | Minils.Wconst sexp -> string_of_static_exp sexp
+  | _ -> "<extvaluedesc constructor not handled>" (* TODO *)
+
+let rec string_of_ck = function
+  | Clocks.Cbase -> "?top"
+  | Clocks.Cvar link -> string_of_link !link
+  | Clocks.Con _ -> "<Clocks.Con not handled>" (* TODO *)
+
+and string_of_link = function
+  | Clocks.Cindex i -> Printf.sprintf "?%d" i
+  | Clocks.Clink ck -> string_of_ck ck (* XXX: Is it right? *)
 
 let string_of_eq (eq: Minils.eq) =
   (* TODO: handle types *)
