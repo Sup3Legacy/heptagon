0ccbc12dd79aeb888bad33e812807a7a8bc824bc
Valentin Lorentz <valentin.lorentz@ens-lyon.org>
2015-06-08 17:33:08 +0200

    [etac] Add merge support.

--- compiler/minils/etac.ml
+++ compiler/minils/etac.ml
@@ -14,8 +14,30 @@ let fresh_clk =
 let types_tbl = Hashtbl.create 256
 let constructors_tbl = Hashtbl.create 1024
 
+let get_type_of_constructor (constructor: Names.constructor_name) =
+  Hashtbl.find constructors_tbl constructor
+
+let add_constructor_type (c: Names.constructor_name) (t: Names.qualname) =
+  Hashtbl.add constructors_tbl c t
+
+let bool_type = {Names.qual= Names.Pervasives; Names.name= "bool"}
+
+let _ =
+  add_constructor_type
+    {Names.qual= Names.Pervasives; Names.name= "true"}
+    bool_type
+let _ =
+  add_constructor_type
+    {Names.qual= Names.Pervasives; Names.name= "false"}
+    bool_type
+let _ =
+  Hashtbl.add types_tbl bool_type (Minils.Type_enum [
+    {Names.qual= Names.Pervasives; Names.name= "true"};
+    {Names.qual= Names.Pervasives; Names.name= "false"}])
+
+
 let int_of_constructor (name: Names.constructor_name) =
-  let t = Hashtbl.find constructors_tbl name in
+  let t = get_type_of_constructor name in
   match (Hashtbl.find types_tbl t) with
   | Minils.Type_enum l ->
       let rec find (i: int) = function
@@ -102,7 +124,7 @@ let rec push_enum_switch eqn_acc clk_acc var_name (constructor: Names.constructo
   match constructors with
   | [] -> (eqn_acc, clk_acc, fresh_clk "")
   | hd :: tl ->
-    let tmpvar = (fresh_var (Printf.sprintf "%s_%s" (string_of_qualname hd) var_name)) in
+    let tmpvar = (fresh_var (Printf.sprintf "clk_%s_%s" (string_of_qualname hd) var_name)) in
     let tmpclk = (fresh_clk (Printf.sprintf "%s_%s" (string_of_qualname hd) var_name)) in
     let vareqn = Printf.sprintf "i1 %s = cmp eq %s %d" tmpvar var_name (int_of_constructor constructor) in
     let clkeqn = Printf.sprintf "%s is %s" tmpclk tmpvar in
@@ -114,7 +136,22 @@ let rec push_enum_switch eqn_acc clk_acc var_name (constructor: Names.constructo
     else
       push_enum_switch eqn_acc clk_acc var_name constructor tl
 
-let rec push_exp acc(exp: Minils.exp)  =
+let rec push_merge eqn_acc clk_acc var_acc var_name pairs =
+  match pairs with
+  | [] -> (eqn_acc, clk_acc, var_acc)
+  | (constructor, exp) :: tl ->
+    let tmpclkvar = (fresh_var (Printf.sprintf "clk_%s_%s" (string_of_qualname constructor) var_name)) in
+    let tmpclk = (fresh_clk (Printf.sprintf "%s_%s" (string_of_qualname constructor) var_name)) in
+    let clkvareqn = Printf.sprintf "i1 %s = cmp eq %s %d" tmpclkvar var_name (int_of_constructor constructor) in
+    let clkeqn = Printf.sprintf "%s is %s" tmpclk tmpclkvar in
+    let clk_acc = tmpclk :: clk_acc in
+    let tmpvar = (fresh_var (Printf.sprintf "%s_%s" (string_of_qualname constructor) var_name)) in
+    let (eqn_acc, value) = push_extvalue eqn_acc exp in
+    let var_eqn = Printf.sprintf "%s = sample %s when %s" tmpvar value tmpclkvar in
+    let eqn_acc = var_eqn :: clkvareqn :: clkeqn :: eqn_acc in
+    push_merge eqn_acc (tmpvar :: var_acc) clk_acc var_name tl
+
+and push_exp acc(exp: Minils.exp)  =
   push_edesc acc exp.Minils.e_desc
 
 and push_edesc acc = function
@@ -124,7 +161,7 @@ and push_edesc acc = function
       push_app acc params app
   | Minils.Ewhen (exp, constructor, var) ->
       let (acc, exp) = push_exp acc exp in
-      let (acc, clks, clk) = (match (Hashtbl.find types_tbl (Hashtbl.find constructors_tbl constructor)) with
+      let (acc, clks, clk) = (match (Hashtbl.find types_tbl (get_type_of_constructor constructor)) with
       | Minils.Type_enum l ->
         push_enum_switch acc [] (string_of_varident var) constructor l
       | _ -> failwith "Constructor of non-enum"
@@ -132,6 +169,17 @@ and push_edesc acc = function
       let unioneqn = Printf.sprintf "?top = %s" (String.concat " | " clks) in
       (* XXX: Should it actually be ?top ? *)
       (unioneqn :: acc, Printf.sprintf "%s when %s" exp clk)
+  | Minils.Emerge (var, l) ->
+      let (first_constructor, _) = (List.hd l) in
+      let t = get_type_of_constructor first_constructor in
+      let (acc, variables, clks) = (match (Hashtbl.find types_tbl t) with (* TODO: empty enums? *)
+      | Minils.Type_enum _ ->
+        push_merge acc [] [] (string_of_varident var) l
+      | _ -> failwith "Constructor of non-enum"
+      ) in
+      let unioneqn = Printf.sprintf "?top = %s" (String.concat " | " clks) in
+      (* XXX: Should it actually be ?top ? *)
+      (unioneqn :: acc, Printf.sprintf "phi %s" (String.concat ", " variables))
   | _ -> (acc, "<edesc constructor not handled>") (* TODO *)
 
 and push_app acc (params: string list) (app: Minils.app) =
@@ -176,7 +224,7 @@ let rec push_ck acc = function
   | Clocks.Cvar link -> push_link acc !link
   | Clocks.Con (ck, constructor, var) ->
       let (acc, ck) = push_ck acc ck in
-      let (acc, clks, clk) = (match (Hashtbl.find types_tbl (Hashtbl.find constructors_tbl constructor)) with
+      let (acc, clks, clk) = (match (Hashtbl.find types_tbl (get_type_of_constructor constructor)) with
       | Minils.Type_enum l ->
         push_enum_switch acc [] (string_of_varident var) constructor l
       | _ -> failwith "Constructor of non-enum"
@@ -226,7 +274,7 @@ let program_pred file (p: Minils.program_desc) = match p with
   | Minils.Ptype t -> (
       match t.Minils.t_desc with
       | Minils.Type_enum l -> ignore (
-          List.map (fun c -> Hashtbl.add constructors_tbl c t.Minils.t_name) l)
+          List.map (fun c -> add_constructor_type c t.Minils.t_name) l)
       | _ -> failwith "Unsupported type"
       );
       Hashtbl.add types_tbl t.Minils.t_name t.Minils.t_desc
